4071b4d4cdea66e3db7eadb8043c23ff84305c93
thm2kor <thiyagarajan.masilamani@gmail.com>
2021-01-10 21:52:00 +0100

    added histogram support to thresholdedImage class

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse
 import config
 import os
 import numpy as np
-import imageio 
+import imageio
 from moviepy.editor import VideoFileClip
 
 #project specific modules
@@ -22,8 +22,8 @@ test_images_path = 'test_images/*.jpg'
 class pipeline:
     def __init__(self):
         # Load the calibration parameters from the pickle file
-        self.cc = cameraCalibration(pickle_file_path) 
-        # track object encapsulates the lane lines detection and the 
+        self.cc = cameraCalibration(pickle_file_path)
+        # track object encapsulates the lane lines detection and the
         # calculation of the intercepts, lane width and vehicle pos
         self.track = drivingLane()
         # pt encapsulates the transform matrices, warp and unwarp function
@@ -43,7 +43,7 @@ class pipeline:
         self.track.detect_lines(self.edges)
         #overlay the tracks on the distorted image
         filled_track = self.track.overlay_lanes(self.distorted, self.edges)
-        #unwarp the combined image        
+        #unwarp the combined image
         if config.debug_mode == True and mode == 0:#1= video mode
             #result = self.show_debug_info(self.edges)
             result = self.prepare_side_by_side()
@@ -59,12 +59,12 @@ class pipeline:
             #return result
             #result = self.show_debug_info(self.edges)
             return result
-    
+
     #Adds the calculated radius of curvature and vehicle position on the video frames
     def add_header(self, image):
         result = np.copy(image)
         font = cv2.FONT_HERSHEY_SIMPLEX
-        
+
         radius = self.track.get_curve_radius()
         position = self.track.get_vehicle_pos(image.shape)
         if radius > 2000:
@@ -72,57 +72,58 @@ class pipeline:
         else:
             text = 'Radius of curvature: ' + '{:05.2f}'.format(radius) + ' m'
         cv2.putText(result, text, (50,70), font, 1.5, (255,255,255), 2, cv2.LINE_AA)
-        
+
         #text = 'Radius of curvature: L ' + '{:05.2f}'.format(self.track.leftline.radius_of_curvature) + 'm'
         #cv2.putText(result, text, (50, 120), font, 1.0, (255,255,255), 2, cv2.LINE_AA)
         #text = 'Radius of curvature: R ' + '{:05.2f}'.format(self.track.rightline.radius_of_curvature) + 'm'
         #cv2.putText(result, text, (50, 170), font, 1.0, (255,255,255), 2, cv2.LINE_AA)
-        
+
         if position > 0:
             direction = 'right of center'
         else:
             direction = 'left of center'
-        text = 'Vehicle is {:04.2f}'.format(abs(position)) + 'm ' + direction 
+        text = 'Vehicle is {:04.2f}'.format(abs(position)) + 'm ' + direction
         cv2.putText(result, text, (50,120), font, 1.5, (255,255,255), 2, cv2.LINE_AA)
-        
+
         return result
-    
+
     ##Functions which draws the left and right line fits on the edges
     def show_debug_info(self, edges):
         debug_bin = np.dstack((edges*255, edges*255, edges*255))
-        
+
         # overhead with all fits added (bottom right)
         debug_bin_points = np.copy(debug_bin)
-        
+
         debug_bin_points[self.track.leftline.ally, self.track.leftline.allx] = [255, 0, 0]
         debug_bin_points[self.track.rightline.ally, self.track.rightline.allx] = [0, 0, 255]
-        
+
         for i, fit in enumerate(self.track.leftline.current_fit):
             debug_bin_points = self.draw_lines_on_image(debug_bin_points, fit, (155,0, 0), 3)
         for i, fit in enumerate(self.track.rightline.current_fit):
             debug_bin_points = self.draw_lines_on_image(debug_bin_points, fit, (0, 0, 155), 3)
-        
+
         debug_bin_points = self.draw_lines_on_image(debug_bin_points, self.track.leftline.best_fit, (0,255,0))
         debug_bin_points = self.draw_lines_on_image(debug_bin_points, self.track.rightline.best_fit, (0,255,0))
-        
+
         return debug_bin_points
-    
+
     def prepare_side_by_side(self, width=1280, height=720):
         font = cv2.FONT_HERSHEY_SIMPLEX
         result = np.zeros((int(height/2), width, 3), dtype=np.uint8)
         edges_bin = np.dstack((self.edges*255, self.edges*255, self.edges*255))
-        combined_bin = np.dstack((self.binary.combined*255, self.binary.combined*255, self.binary.combined*255))
-        
+        histogram = self.binary.getHistogram()
+        hist_bin = np.dstack((histogram*255, histogram*255, histogram*255))
+
         result[0:int(height/2), 0:int(width/2), : ] = cv2.resize( edges_bin, (int(width/2) ,int(height/2)))
-        text = 'Edges detected with CLAHE preprocessing + thresholding HSV and RGB'
-        cv2.putText(result, text, (50,40), font, 0.5, (0,255,0), 2, cv2.LINE_AA)
-        
-        result[0:int(height/2), int(width/2):width, :] = cv2.resize( combined_bin, (int(width/2) ,int(height/2)))
         text = 'Edges detected with combined results from Luv(l) and Lab(b)'
+        cv2.putText(result, text, (50,40), font, 0.5, (0,255,0), 2, cv2.LINE_AA)
+
+        result[0:int(height/2), int(width/2):width, :] = cv2.resize( hist_bin, (int(width/2) ,int(height/2)))
+        text = 'Histogram'
         cv2.putText(result, text, (int((width/2)+50),40), font, 0.5, (0,255,0), 2, cv2.LINE_AA)
-        
+
         return result
-        
+
     #Helper function for debug info - draws a given line on to an image
     def draw_lines_on_image(self, image, fit, plot_color, thickness=5):
         if fit is None:
@@ -138,30 +139,31 @@ class pipeline:
 # function which reads in a video and prepares the frames for the pipeline
 def processVideo(filename, start, end):
     clip = VideoFileClip(filename)
-    if config.debug_mode:
+    if start > 0 and end > 0:
         print('processing input video from ' + str(start) + ' to ' + str(end) + ' seconds')
         clip = clip.subclip(start,end)
-    snapshot = clip.fl_image(pipeline() ) 
-    
+    snapshot = clip.fl_image(pipeline() )
+
     if config.debug_mode == False:
         result_file_name = 'result_' + os.path.basename(filename)
-    else:        
-        result_file_name = 'debug_result_' + os.path.basename(filename)        
+    else:
+        result_file_name = 'debug_result_' + os.path.basename(filename)
     snapshot.write_videofile(output_path_video + result_file_name, audio=False)
     return
-    
-# function which reads in a image file and prepares the frames for the pipeline 
+
+# function which reads in a image file and prepares the frames for the pipeline
 def processImage(filename):
     #read in the file and flipping the color from BGR to RGB
-    image = cv2.cvtColor(mpimg.imread(filename), cv2.COLOR_BGR2RGB)   
+    image = cv2.cvtColor(mpimg.imread(filename), cv2.COLOR_BGR2RGB)
     pl = pipeline()
     result = pl(image,1)
-    
-    mpimg.imsave( output_path + 'result_' + os.path.basename(filename) , cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+
+    #mpimg.imsave( output_path + 'result_' + os.path.basename(filename) , cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+    mpimg.imsave( output_path + 'result_' + os.path.basename(filename) , result)
     print ('Results saved at ' + output_path + 'result_' + os.path.basename(filename) )
 
 #start-up function - Reads in the program arguments and prepares the respective pipelines
-def main(): 
+def main():
     # from a conda environment, call 'python main.py <full-path-to-image-file>
     parser = argparse.ArgumentParser(description='Detect lanes and highlight them')
     parser.add_argument('filename')
@@ -175,17 +177,20 @@ def main():
     start = 0
     end = 5
     time = []
-    
+
     if time_count is not None:
-        time = [int(i) for i in time_count.split('-') if i.isdigit()]         
+        time = [int(i) for i in time_count.split('-') if i.isdigit()]
         if len(time) == 2:
-            start = time[0]            
+            start = time[0]
             end = time[1]
-    
+    else:
+        start = -1
+        end = -1
+
     if args.filename[-4:] == '.mp4':
         processVideo(args.filename, start, end)
     else: ##assuming image
-        processImage (args.filename) 
-            
+        processImage (args.filename)
+
 if __name__ == '__main__':
     main()
