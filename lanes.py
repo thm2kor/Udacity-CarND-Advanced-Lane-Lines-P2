5d74f7a2e1486538000fa72be4ce9a9a82234c8e
thm2kor <thiyagarajan.masilamani@gmail.com>
2021-01-10 21:58:54 +0100

    command line --timeslot can be úsed in normal mode as well

--- lanes.py
+++ lanes.py
@@ -6,23 +6,23 @@ class line():
     def __init__(self, id=""):
         self.id = id
         # was the line detected in the last iteration?
-        self.detected = False  
+        self.detected = False
         # x values of the last n fits of the line
-        self.recent_xfitted = [] 
+        self.recent_xfitted = []
         #x values of the fitted line over the last n iterations
-        self.bestx = None     
+        self.bestx = None
         #polynomial coefficients averaged over the last n iterations
-        self.best_fit = None  
+        self.best_fit = None
         #polynomial coefficients for the last n number of runs
-        self.current_fit = []  
+        self.current_fit = []
         #recent calculated polynomial coefficients
         self.recent_fit = None
         #radius of curvature of the line in some units
-        self.radius_of_curvature = None 
+        self.radius_of_curvature = None
         #distance in meters of vehicle center from the line
-        self.line_base_pos = None 
+        self.line_base_pos = None
         #difference in fit coefficients between last and new fits
-        self.diffs = np.array([0,0,0], dtype='float') 
+        self.diffs = np.array([0,0,0], dtype='float')
         #number of detected pixels
         self.px_count = None ## unused
         #color of the line
@@ -35,25 +35,25 @@ class line():
         self.ally = []
         #x values for detected line pixels
         self.allx = []
-        
-    ## calculate the radis of curvature     
+
+    ## calculate the radis of curvature
     def calc_curavture(self, ym_per_pix, xm_per_pix ):
         ploty = np.linspace(0, (config.IMAGE_HEIGHT)-1, config.IMAGE_HEIGHT)
         #fit a second degree polynomial, which fits the current x and y points
         fit_cr = np.polyfit((self.ally * ym_per_pix), (self.allx * xm_per_pix), 2)
         #fit a second degree polynomial, which fits the current x and y points
         y_eval = np.max(ploty)*ym_per_pix
-        
-        self.radius_of_curvature = ((1 + (2*fit_cr[0]*y_eval + fit_cr[1])**2)**1.5)/abs(2*fit_cr[0]) 
+
+        self.radius_of_curvature = ((1 + (2*fit_cr[0]*y_eval + fit_cr[1])**2)**1.5)/abs(2*fit_cr[0])
         return self.radius_of_curvature
-    
+
     # This function validates the "recent-fit" which was set by the 'track' object.
     # The line object saves the last n fits. If the incoming fit is deviating too much
     # from the last n fits, then the line rejects the fit and sets the internal state
-    # 'detected' to false. This is an indication to the track object to re-scan 
+    # 'detected' to false. This is an indication to the track object to re-scan
     # the lanes using the sliding window approach.
     def validate_recent_fit(self):
-        # if the recent fit is valid.  
+        # if the recent fit is valid.
         # The track object can invalidate the recent fit by setting it to None
         if self.recent_fit is not None:
             if self.best_fit is not None:
@@ -64,7 +64,7 @@ class line():
                 self.detected = False
             else:
                 self.detected = True
-                # All ok. Add to the curent fit 
+                # All ok. Add to the curent fit
                 self.current_fit.append(self.recent_fit)
                 ## Ranking of the current fit based on the number of detected pixels
                 self.ranking.append(len(self.recent_xfitted))
@@ -79,7 +79,7 @@ class line():
             if len(self.current_fit) > 0:
                 self.best_fit = np.average(self.current_fit, axis=0, weights=self.ranking)
 
-class drivingLane: 
+class drivingLane:
     def __init__(self) :
         self.leftline = line ('left')
         self.rightline = line ('right')
@@ -89,23 +89,23 @@ class drivingLane:
         ##From class notes Lesson 8 - Advanced Computer vision - Measuring Curvature -II
         ##"U.S. regulations require a minimum lane width of 12 feet or 3.7 meters,
         ## and the dashed lane lines are 10 feet or 3 meters long each."
-        
-        ## The above dimensions in world space is manually checked with an example warped 
+
+        ## The above dimensions in world space is manually checked with an example warped
         ## image - straight_line1.jpg and straight_line2.jpg
-    	self.ym_per_pix = 3/110 	# 110 is the number of pixels for one lane segement in straight_line1_warped.jpg
+        self.ym_per_pix = 3/110 	# 110 is the number of pixels for one lane segement in straight_line1_warped.jpg
         self.xm_per_pix = 3.7/380	# 380 is number of pixels for one lane width in straight_line1_warped.jpg
 
-        
+
     def is_detected(self):
         return self.leftline.detected and self.rightline.detected
-    
+
     def is_incoming_fit_valid(self):
         return self.leftline.incoming_fit is not None and self.rightline.incoming_fit is not None
-        
+
     def get_curve_radius(self):
         return (self.leftline.calc_curavture(self.ym_per_pix, self.xm_per_pix) + \
                 self.rightline.calc_curavture(self.ym_per_pix, self.xm_per_pix))/2
-    
+
     #function compares the left lane and right lane at 3 identical y coordinates.
     #if the average difference of the left and right lane are within a certain range
     #then it can be detected that the lanes are parallel to each other
@@ -113,22 +113,22 @@ class drivingLane:
         ret = True
         if self.leftline.recent_fit is not None and self.rightline.recent_fit is not None:
             widths = np.zeros(3) # calculate widths at top, mid and bottom of the frame (perspective transformed)
-            
+
             ploty = np.linspace(0, config.IMAGE_WIDTH-1, num=config.IMAGE_WIDTH)# to cover same y-range as image
             left_fitx = self.leftline.recent_fit[0]*ploty**2 + self.leftline.recent_fit[1]*ploty + self.leftline.recent_fit[2]
             right_fitx = self.rightline.recent_fit[0]*ploty**2 + self.rightline.recent_fit[1]*ploty + self.rightline.recent_fit[2]
-    
+
             if left_fitx is not None and right_fitx is not None:
                 widths[0] = abs(right_fitx[0] - left_fitx[0])
                 widths[1] = abs(right_fitx[int(max(ploty)/2)] - left_fitx[int(max(ploty)/2)])
                 widths[2] = abs(right_fitx[int(max(ploty)-1)] - left_fitx[int(max(ploty)-1)])
                 #print (widths)
-            
+
             if max(widths) - min(widths) >= 100 : #TODO - Finetune the thresholds by running the challenge video
                 ret = True
-            
+
         return ret
-    
+
     #function calculates the intercept and left and right lane
     #the difference of the intercept is the lane width.
     #if the lane width is longer than  width + offset, then
@@ -141,35 +141,35 @@ class drivingLane:
             right_intercept = self.rightline.recent_fit[0]*config.IMAGE_HEIGHT**2 + self.rightline.recent_fit[1]*config.IMAGE_HEIGHT + self.rightline.recent_fit[2]
             x_int_diff = abs(right_intercept-left_intercept)
             if x_int_diff > 480 : #(380 + offset of 100)
-                ret = True            
+                ret = True
         return ret
-    
+
     #trigger the calculation of the radius of curvature of the individual lines
     def calc_curvature(self):
         self.leftline.calc_curavture(self.ym_per_pix, self.xm_per_pix)
         self.rightline.calc_curavture(self.ym_per_pix, self.xm_per_pix)
-    
+
     #calculation the vehicle position with respect to the lane
     def get_vehicle_pos(self, image_shape):
-        ## Class notes -- You can assume the camera is mounted at the center of the car, 
+        ## Class notes -- You can assume the camera is mounted at the center of the car,
         ## such that the lane center is the midpoint at the bottom of the image
-        ## between the two lines you've detected. The offset of the lane 
-        ## center from the center of the image (converted from pixels to meters) 
+        ## between the two lines you've detected. The offset of the lane
+        ## center from the center of the image (converted from pixels to meters)
         ##is your distance from the center of the lane.
-        ## https://knowledge.udacity.com/questions/311566        
+        ## https://knowledge.udacity.com/questions/311566
         vehicle_position = image_shape[1]/2
         leftline_intercept = self.leftline.best_fit[0]*image_shape[0]**2 + self.leftline.best_fit[1]*image_shape[0] + self.leftline.best_fit[2]
         rightline_intercept = self.rightline.best_fit[0]*image_shape[0]**2 + self.rightline.best_fit[1]*image_shape[0] + self.rightline.best_fit[2]
         lane_center = (leftline_intercept + rightline_intercept) /2
         self.vehicle_pos = (vehicle_position - lane_center) * self.xm_per_pix
         return self.vehicle_pos
-    
+
     #Function that detects potential lane lines from an warped, binary thresholded
     #image.
     def detect_lines(self, edges):
         if not self.is_detected():
             # attempts the find a set of lines based on "sliding windows method"
-            self.find_new_fit(edges) 
+            self.find_new_fit(edges)
         else:
             # find the lane around the points detected from the previous function
             self.follow_prev_fit(edges)
@@ -181,11 +181,11 @@ class drivingLane:
         # validity check at line level
         self.leftline.validate_recent_fit()
         self.rightline.validate_recent_fit()
-    
-    # Finding a lane based on the sliding windows  method. 
+
+    # Finding a lane based on the sliding windows  method.
     # code taken over from Lesson : Finding the Lines: Sliding Window
     # Chapter 8: Advanced Computer Vision
-    # from Udacity Nanodegree program : 
+    # from Udacity Nanodegree program :
     # Minor modifications done for encapsulating the function inside a class
     def find_new_fit(self, img):
         # Take a histogram of the bottom half of the image
@@ -196,7 +196,7 @@ class drivingLane:
         #quarter_point = np.int(midpoint//2)
         # Previously the left/right base was the max of the left/right half of the histogram
         # this changes it so that only a quarter of the histogram (directly to the left/right) is considered
-        leftx_base = np.argmax(histogram[:midpoint]) 
+        leftx_base = np.argmax(histogram[:midpoint])
         rightx_base = np.argmax(histogram[midpoint:]) + midpoint
         # Choose the number of sliding windows
         nwindows = 10
@@ -215,7 +215,7 @@ class drivingLane:
         minpix = 30
         # Create empty lists to receive left and right lane pixel indices
         left_lane_inds = []
-        right_lane_inds = []    
+        right_lane_inds = []
         # Step through the windows one by one
         for window in range(nwindows):
             # Identify window boundaries in x and y (and right and left)
@@ -234,19 +234,19 @@ class drivingLane:
             # If you found > minpix pixels, recenter next window on their mean position
             if len(good_left_inds) > minpix:
                 leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
-            if len(good_right_inds) > minpix:        
+            if len(good_right_inds) > minpix:
                 rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
-    
+
         # Concatenate the arrays of indices
         left_lane_inds = np.concatenate(left_lane_inds)
         right_lane_inds = np.concatenate(right_lane_inds)
-    
+
         # Extract left and right line pixel positions
         leftx = nonzerox[left_lane_inds]
-        lefty = nonzeroy[left_lane_inds] 
+        lefty = nonzeroy[left_lane_inds]
         rightx = nonzerox[right_lane_inds]
-        righty = nonzeroy[right_lane_inds] 
-            
+        righty = nonzeroy[right_lane_inds]
+
         # Fit a second order polynomial to each
         if len(leftx) != 0:
             self.leftline.recent_xfitted = leftx
@@ -267,30 +267,30 @@ class drivingLane:
         else:
             self.rightline.recent_fit = None
             #print ('right lane not found')
-    
+
     # Tracking a line based on the previous selected fit.
     # code taken over from Lesson : Finding the Lines: Search from Prior
     # Chapter 8: Advanced Computer Vision
     # Minor modifications done for encapsulating the function inside a class
-    def follow_prev_fit(self,binary_warped): 
+    def follow_prev_fit(self,binary_warped):
         #print ('tracking lane')
         nonzero = binary_warped.nonzero()
         nonzeroy = np.array(nonzero[0])
         nonzerox = np.array(nonzero[1])
-        
+
         margin = 80
-        
-        left_lane_inds = ((nonzerox > (self.leftline.recent_fit[0]*(nonzeroy**2) + self.leftline.recent_fit[1]*nonzeroy + self.leftline.recent_fit[2] - margin)) & 
-                          (nonzerox < (self.leftline.recent_fit[0]*(nonzeroy**2) + self.leftline.recent_fit[1]*nonzeroy + self.leftline.recent_fit[2] + margin))) 
-        right_lane_inds = ((nonzerox > (self.rightline.recent_fit[0]*(nonzeroy**2) + self.rightline.recent_fit[1]*nonzeroy + self.rightline.recent_fit[2] - margin)) & 
-                           (nonzerox < (self.rightline.recent_fit[0]*(nonzeroy**2) + self.rightline.recent_fit[1]*nonzeroy + self.rightline.recent_fit[2] + margin)))  
-    
+
+        left_lane_inds = ((nonzerox > (self.leftline.recent_fit[0]*(nonzeroy**2) + self.leftline.recent_fit[1]*nonzeroy + self.leftline.recent_fit[2] - margin)) &
+                          (nonzerox < (self.leftline.recent_fit[0]*(nonzeroy**2) + self.leftline.recent_fit[1]*nonzeroy + self.leftline.recent_fit[2] + margin)))
+        right_lane_inds = ((nonzerox > (self.rightline.recent_fit[0]*(nonzeroy**2) + self.rightline.recent_fit[1]*nonzeroy + self.rightline.recent_fit[2] - margin)) &
+                           (nonzerox < (self.rightline.recent_fit[0]*(nonzeroy**2) + self.rightline.recent_fit[1]*nonzeroy + self.rightline.recent_fit[2] + margin)))
+
         # Again, extract left and right line pixel positions
         leftx = nonzerox[left_lane_inds]
-        lefty = nonzeroy[left_lane_inds] 
+        lefty = nonzeroy[left_lane_inds]
         rightx = nonzerox[right_lane_inds]
         righty = nonzeroy[right_lane_inds]
-        
+
         if len(leftx) != 0:
             # Fit a second order polynomial to each
             self.leftline.recent_xfitted = leftx
@@ -302,8 +302,8 @@ class drivingLane:
             self.rightline.ally = righty
             self.rightline.allx = rightx
             self.rightline.recent_fit = np.polyfit(righty, rightx, 2)
-    
-    
+
+
     def overlay_lanes(self, original, overlay):
         #new_img = np.copy(original_img)
         if self.leftline.best_fit is None or self.rightline.best_fit is None:
@@ -312,7 +312,7 @@ class drivingLane:
         # Create an image to draw the lines on
         warp_zero = np.zeros_like(overlay).astype(np.uint8)
         color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
-        
+
         ploty = np.linspace(0, overlay.shape[0]-1, num=overlay.shape[0])# to cover same y-range as image
         left_fitx = self.leftline.best_fit[0]*ploty**2 + self.leftline.best_fit[1]*ploty + self.leftline.best_fit[2]
         right_fitx = self.rightline.best_fit[0]*ploty**2 + self.rightline.best_fit[1]*ploty + self.rightline.best_fit[2]
@@ -321,10 +321,10 @@ class drivingLane:
         pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
         pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
         pts = np.hstack((pts_left, pts_right))
-    
+
         # Draw the lane onto the warped blank image
         cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
         cv2.polylines(color_warp, np.int32([pts_left]), isClosed=False, color=self.leftline.color, thickness=self.leftline.thickness)
         cv2.polylines(color_warp, np.int32([pts_right]), isClosed=False, color=self.rightline.color, thickness=self.rightline.thickness)
-    
+
         return color_warp
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse
 import config
 import os
 import numpy as np
-import imageio 
+import imageio
 from moviepy.editor import VideoFileClip
 
 #project specific modules
@@ -22,8 +22,8 @@ test_images_path = 'test_images/*.jpg'
 class pipeline:
     def __init__(self):
         # Load the calibration parameters from the pickle file
-        self.cc = cameraCalibration(pickle_file_path) 
-        # track object encapsulates the lane lines detection and the 
+        self.cc = cameraCalibration(pickle_file_path)
+        # track object encapsulates the lane lines detection and the
         # calculation of the intercepts, lane width and vehicle pos
         self.track = drivingLane()
         # pt encapsulates the transform matrices, warp and unwarp function
@@ -43,7 +43,7 @@ class pipeline:
         self.track.detect_lines(self.edges)
         #overlay the tracks on the distorted image
         filled_track = self.track.overlay_lanes(distorted, self.edges)
-        #unwarp the combined image        
+        #unwarp the combined image
         if config.debug_mode == True and mode == 0:#1= video mode
             result = self.show_debug_info(self.edges)
             #in the debug mode, only return edges and the line fits
@@ -57,12 +57,12 @@ class pipeline:
             result = self.add_header(result)
             #return result
             return result
-    
+
     #Adds the calculated radius of curvature and vehicle position on the video frames
     def add_header(self, image):
         result = np.copy(image)
         font = cv2.FONT_HERSHEY_SIMPLEX
-        
+
         radius = self.track.get_curve_radius()
         position = self.track.get_vehicle_pos(image.shape)
         if radius > 2000:
@@ -70,41 +70,41 @@ class pipeline:
         else:
             text = 'Radius of curvature: ' + '{:05.2f}'.format(radius) + ' m'
         cv2.putText(result, text, (50,70), font, 1.5, (255,255,255), 2, cv2.LINE_AA)
-        
+
         #text = 'Radius of curvature: L ' + '{:05.2f}'.format(self.track.leftline.radius_of_curvature) + 'm'
         #cv2.putText(result, text, (50, 120), font, 1.0, (255,255,255), 2, cv2.LINE_AA)
         #text = 'Radius of curvature: R ' + '{:05.2f}'.format(self.track.rightline.radius_of_curvature) + 'm'
         #cv2.putText(result, text, (50, 170), font, 1.0, (255,255,255), 2, cv2.LINE_AA)
-        
+
         if position > 0:
             direction = 'right of center'
         else:
             direction = 'left of center'
-        text = 'Vehicle is {:04.2f}'.format(abs(position)) + 'm ' + direction 
+        text = 'Vehicle is {:04.2f}'.format(abs(position)) + 'm ' + direction
         cv2.putText(result, text, (50,120), font, 1.5, (255,255,255), 2, cv2.LINE_AA)
-        
+
         return result
-    
+
     ##Functions which draws the left and right line fits on the edges
     def show_debug_info(self, edges):
         debug_bin = np.dstack((edges*255, edges*255, edges*255))
-        
+
         # overhead with all fits added (bottom right)
         debug_bin_points = np.copy(debug_bin)
-        
+
         debug_bin_points[self.track.leftline.ally, self.track.leftline.allx] = [255, 0, 0]
         debug_bin_points[self.track.rightline.ally, self.track.rightline.allx] = [0, 0, 255]
-        
+
         for i, fit in enumerate(self.track.leftline.current_fit):
             debug_bin_points = self.draw_lines_on_image(debug_bin_points, fit, (155,0, 0), 3)
         for i, fit in enumerate(self.track.rightline.current_fit):
             debug_bin_points = self.draw_lines_on_image(debug_bin_points, fit, (0, 0, 155), 3)
-        
+
         debug_bin_points = self.draw_lines_on_image(debug_bin_points, self.track.leftline.best_fit, (0,255,0))
         debug_bin_points = self.draw_lines_on_image(debug_bin_points, self.track.rightline.best_fit, (0,255,0))
-        
+
         return debug_bin_points
-    
+
     #Helper function for debug info - draws a given line on to an image
     def draw_lines_on_image(self, image, fit, plot_color, thickness=5):
         if fit is None:
@@ -120,29 +120,29 @@ class pipeline:
 # function which reads in a video and prepares the frames for the pipeline
 def processVideo(filename, start, end):
     clip = VideoFileClip(filename)
-    if config.debug_mode:
+    if start > 0 and end > 0:
         clip = clip.subclip(start,end)
-    snapshot = clip.fl_image(pipeline() ) 
-    
+    snapshot = clip.fl_image(pipeline() )
+
     if config.debug_mode == False:
         result_file_name = 'result_' + os.path.basename(filename)
-    else:        
-        result_file_name = 'debug_result_' + os.path.basename(filename)        
+    else:
+        result_file_name = 'debug_result_' + os.path.basename(filename)
     snapshot.write_videofile(output_path_video + result_file_name, audio=False)
     return
-    
-# function which reads in a image file and prepares the frames for the pipeline 
+
+# function which reads in a image file and prepares the frames for the pipeline
 def processImage(filename):
     #read in the file and flipping the color from BGR to RGB
-    image = cv2.cvtColor(mpimg.imread(filename), cv2.COLOR_BGR2RGB)   
+    image = cv2.cvtColor(mpimg.imread(filename), cv2.COLOR_BGR2RGB)
     pl = pipeline()
     result = pl(image,1)
-    
+
     mpimg.imsave( output_path + 'result_' + os.path.basename(filename) , cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
     print ('Results saved at ' + output_path + 'result_' + os.path.basename(filename) )
 
 #start-up function - Reads in the program arguments and prepares the respective pipelines
-def main(): 
+def main():
     # from a conda environment, call 'python main.py <full-path-to-image-file>
     parser = argparse.ArgumentParser(description='Detect lanes and highlight them')
     parser.add_argument('filename')
@@ -151,22 +151,25 @@ def main():
     #read command line agruments
     args = parser.parse_args()
     config.debug_mode = args.debug_mode
-    # used only in debug mode --frames='start'-'end'
+
     time_count = args.timeslot
     start = 0
     end = 5
     time = []
-    
+
     if time_count is not None:
-        time = [int(i) for i in time_count.split('-') if i.isdigit()]         
+        time = [int(i) for i in time_count.split('-') if i.isdigit()]
         if len(time) == 2:
-            start = time[0]            
+            start = time[0]
             end = time[1]
-    
+    else:
+        start = -1
+        end = -1
+
     if args.filename[-4:] == '.mp4':
         processVideo(args.filename, start, end)
     else: ##assuming image
-        processImage (args.filename) 
-            
+        processImage (args.filename)
+
 if __name__ == '__main__':
     main()
